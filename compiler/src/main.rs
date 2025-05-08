1c841db620c528e796b3e11f7bfa20a69ca567e8
William McPherson <willmcpherson2@gmail.com>
2025-05-08 21:53:19 +1000

    Try to use standard Rust in runtime
    The JIT segfaults.

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let module = Module::parse_bitcode_from_buffer(&buffer, &context).unwrap();
     let builder = context.create_builder();
 
-    let rts_fun_names = HashSet::from([c"foo", c"bar"]);
+    let rts_fun_names = HashSet::from([c"new_foo", c"free_foo", c"sum_foo"]);
     let rts_funs = module
         .get_functions()
         .filter(|fun| rts_fun_names.contains(fun.get_name()));
@@ -29,14 +29,28 @@ fn main() {
     let block = context.append_basic_block(main_fun, "start");
     builder.position_at_end(block);
 
-    let fun = module.get_function("foo").unwrap();
-    let arg = context.i32_type().const_int(42, false);
-    let result = builder
-        .build_call(fun, &[arg.into()], "result")
+    let new_foo = module.get_function("new_foo").unwrap();
+    let foo = builder
+        .build_call(new_foo, &[], "foo")
         .unwrap()
         .try_as_basic_value()
-        .unwrap_left();
+        .unwrap_left()
+        .into_pointer_value();
+
+    let sum_foo = module.get_function("sum_foo").unwrap();
+    let result = builder
+        .build_call(sum_foo, &[foo.into()], "result")
+        .unwrap()
+        .try_as_basic_value()
+        .unwrap_left()
+        .into_int_value();
+
+    let free_foo = module.get_function("free_foo").unwrap();
+    builder.build_call(free_foo, &[foo.into()], "").unwrap();
+
     builder.build_return(Some(&result)).unwrap();
+
+    module.verify().unwrap();
 
     binary(&module);
 
@@ -45,12 +59,10 @@ fn main() {
 
 fn jit(module: &Module) -> i32 {
     let engine = module
-        .create_jit_execution_engine(OptimizationLevel::Default)
+        .create_jit_execution_engine(OptimizationLevel::None)
         .unwrap();
-    module.print_to_stderr();
-    type MainFun = unsafe extern "C" fn() -> i32;
-    let main_fun = unsafe { engine.get_function::<MainFun>("main") }.unwrap();
-    unsafe { main_fun.call() }
+    let main_fun = module.get_function("main").unwrap();
+    unsafe { engine.run_function_as_main(main_fun, &[]) }
 }
 
 fn binary(module: &Module) {
@@ -62,7 +74,7 @@ fn binary(module: &Module) {
             &target_triple,
             "generic",
             "",
-            OptimizationLevel::Default,
+            OptimizationLevel::None,
             RelocMode::Default,
             CodeModel::Default,
         )
--- rts/src/lib.rs
+++ rts/src/lib.rs
@@ -1,6 +1,22 @@
-#[no_mangle]
-pub extern "C" fn foo(n: i32) -> i32 {
-    n * 2
+struct Foo {
+    numbers: Vec<i32>,
+}
+
+impl Foo {
+    #[no_mangle]
+    pub extern "C" fn new_foo() -> Box<Self> {
+        Box::new(Foo {
+            numbers: vec![1, 2, 3],
+        })
+    }
+
+    #[no_mangle]
+    pub extern "C" fn free_foo(_foo: Box<Foo>) {}
+
+    #[no_mangle]
+    pub extern "C" fn sum_foo(&self) -> i32 {
+        self.numbers.iter().sum()
+    }
 }
 
 #[cfg(test)]
@@ -9,6 +25,8 @@ mod test {
 
     #[test]
     fn test_foo() {
-        assert_eq!(foo(42), 84)
+        let foo = Foo::new_foo();
+        assert_eq!(foo.sum_foo(), 6);
+        Foo::free_foo(foo);
     }
 }
